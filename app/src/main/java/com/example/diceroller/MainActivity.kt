1d9a27ce6b108ddcd4964475b12511315dc42d81
mdshahariazrimon <mriaz182048@gmail.com>
2023-01-17 21:57:38 +0600

    Change The Text

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,8 +14,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_btn)
         //rollButton.text = "Let's Roll"
+
+        /*
         rollButton.setOnClickListener {
             Toast.makeText(this,"Button Clicked", Toast.LENGTH_SHORT).show()
         }
+        */
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.result_txt)
+        val randomInt = Random.nextInt(6)+1
+        resultText.text= randomInt.toString()
     }
 }
